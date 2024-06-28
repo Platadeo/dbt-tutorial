c4d1287b58e36b63581baf9bcbda4a0056181274
Platadeo <platadeo.atug@gmail.com>
2024-06-28 12:48:39 -0700

    asd

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,12 +1,28 @@
 with source as (
     select * from {{ ref("raw_payments") }}
 ),
+payments as (
+    select * from {{ ref("raw_orders") }}
+),
 
 renamed as (
     select
-    id as order_id,
-    order_id as user_id,
+    order_id as id,
     amount/100 as amount
-from source)
+from source),
 
-select * from renamed
+customer as (
+    select
+    user_id as customer_id,
+    id as id
+from payments),
+
+final as(
+    select
+    id,
+    customer_id,
+    amount
+    from renamed
+    join customer using (id) 
+    )
+select * from final
